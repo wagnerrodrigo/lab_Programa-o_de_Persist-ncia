77a2d17fb1b09a5e26a4437058be38e30a7c1b29
Wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2019-03-13 20:26:09 -0300

    metodo prepareStatement

--- Exer-Persistencia/CRUD-2019/src/br/cesjf/crud2019/Main.java
+++ Exer-Persistencia/CRUD-2019/src/br/cesjf/crud2019/Main.java
@@ -7,6 +7,8 @@ package br.cesjf.crud2019;
 
 import JDPC.ConectionFactory;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  *
@@ -19,5 +21,26 @@ public class Main {
         // cria a query
         String sql = "INSERT INTO aluno (nome, dataMatricula, enderco, telefone, dataNacimento, altura, peso),"
                 + "VALURES(?, ?, ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement statement = comn.prepareStatement(sql);
+            
+            statement.setString(1, aluno.getNome());
+            statement.setDate(2, new java.sql.Date(aluno.getDataMatricula().getTime()));
+            statement.setString(3, aluno.getEnderco());
+            statement.setString(4, aluno.getTelefone());
+            statement.setDate(5, new java.sql.Date(aluno.getDataNacimento().getTime()));
+            statement.setFloat(6, aluno.getAltura());
+            statement.setFloat(7, aluno.getPeso());
+            
+            int linhasAfetadas = statement.executeUpdate();
+            if (linhasAfetadas>0){
+                System.out.println("Inserção realizada com sucesso");
+            }else{
+                throw new RuntimeException("Erro ao inserir aluno");
+            }
+            
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao inserir aluno!");
+        }
     }
 }
