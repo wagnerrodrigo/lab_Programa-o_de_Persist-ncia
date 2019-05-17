7e68bf16a1732467cf14d7b3ed7fe57a786a7e85
Wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2019-05-16 21:18:18 -0300

    classe de pecistencia no banco

--- Exer-Persistencia/agenda2019/src/modelo/Contato.java
+++ Exer-Persistencia/agenda2019/src/modelo/Contato.java
@@ -20,8 +20,26 @@ public class Contato implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String nome;
+    private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 
     public Long getId() {
         return id;
