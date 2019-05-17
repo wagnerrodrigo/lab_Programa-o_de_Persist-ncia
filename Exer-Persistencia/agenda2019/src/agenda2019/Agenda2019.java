d2924c177371bf46b9875048903985362d32390b
Wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2019-05-16 22:13:03 -0300

    add outros ususarios

--- Exer-Persistencia/agenda2019/src/agenda2019/Agenda2019.java
+++ Exer-Persistencia/agenda2019/src/agenda2019/Agenda2019.java
@@ -6,7 +6,9 @@
 package agenda2019;
 
 import DAO.ContatoDAO;
+import java.util.List;
 import modelo.Contato;
+import org.hibernate.Session;
 
 /**
  *
@@ -24,11 +26,26 @@ public class Agenda2019 {
         ContatoDAO contatodao = new ContatoDAO();
         
         Contato contato = new Contato();
-        contato.setNome("wagner");
-        contato.setTelefone("988031096");
+        Contato contato2 = new Contato();
+        
+//        contato.setNome("wagner");
+//        contato.setTelefone("988031096");
+        
+        // segundo contato
+        
+        contato.setNome("Filipe");
+        contato.setTelefone("988140342");
+        // terceiro contato
+        
+        contato2.setNome("Rafaela");
+        contato2.setTelefone("988033423");
         
         contatodao.persist(contato);
+        contatodao.persist(contato2);
         System.out.println("Finalizar projeto");
+        
+//        Session = contatodao.getCurrentSession();
+//        List<Contato> lista = (List<Contato>)
     }
     
 }
