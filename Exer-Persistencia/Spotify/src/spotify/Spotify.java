3258b1302ab7fa67d50f6658cc06441cbe5f4eff
Wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2019-05-23 21:54:41 -0300

    Criando objetos

--- Exer-Persistencia/Spotify/src/spotify/Spotify.java
+++ Exer-Persistencia/Spotify/src/spotify/Spotify.java
@@ -5,6 +5,12 @@
  */
 package spotify;
 
+import DAO.MusicaDAO;
+import DAO.PlaylistDAO;
+import Dominio.Musica;
+import Dominio.Playlist;
+
+
 /**
  *
  * @author alunoces
@@ -16,6 +22,23 @@ public class Spotify {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Iniciando o Projeto");
+        
+        MusicaDAO musicadao = new MusicaDAO(); // criando o acesso ao Dao da MUSICA
+        PlaylistDAO playlistdao = new PlaylistDAO(); // criando o acesso ao Dao da PLAYLIST
+        
+        Playlist playlist = new Playlist(); // criando o Objeto playlist da classe de Dominio
+        Musica musica = new Musica();  // criando o Objeto Musica da classe de Dominio
+       
+        playlist.setNome("");
+        playlist.setDescricao("Essa é pra quem gosta dos clássicos. Nessa playlist, você encontrará grandes clássicos para os mais saudosistas e para quem deseja conhecer uma pouco mais sobre esse incrível gênero que é o rock n roll. Com incríveis 9 horas de reprodução, você terá uma extensa lista de músicas para escutar e recomendar para os seus amigos. Além de tudo, essa playlist é atualizada constantemente pelo pessoal do Spotify, fazendo com que você nunca caia na mesmice e sempre tenha a oportunidade de descobrir um novo e incrível som.");
+        
+        musica.getPlaylist();
+        musica.getTitulo();
+        musica.getBanda();
+        musica.getNota();
+       
+        
     }
     
 }
