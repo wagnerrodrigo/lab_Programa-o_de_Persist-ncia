a0354e6cf9d52d202579a6e5455ea7dbc9eb7fd2
Wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2019-05-29 20:07:52 -0300

    Criando as Classe de Conexao e ClienteDao, metodo de salvar no banco

---
+++ Exer-Persistencia/Ecommerce/src/DAO/ClienteDao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Dominio.Cliente;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author alunoces
+ */
+public class ClienteDao {
+    public void salvarAtualizar(Cliente cliente){
+        EntityManager em = Conexao.getEntityManager();
+                      em.getTransaction().begin();
+       if(cliente.getId() != null){
+           cliente = em.merge(cliente);
+       }
+       em.persist(cliente);
+       em.getTransaction().commit();
+       em.close();
+    }
+}
