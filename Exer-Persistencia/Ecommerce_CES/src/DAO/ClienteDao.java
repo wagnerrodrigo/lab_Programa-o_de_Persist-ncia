01b122c8985b49fa213435e9dd05fe76e2c4a02c
Wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2019-06-06 21:32:05 -0300

    clienteDao e Conexao no pacote DAO

---
+++ Exer-Persistencia/Ecommerce_CES/src/DAO/ClienteDao.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Dominio.Cliente;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author alunoces
+ */
+public class ClienteDao {
+    public void salvarAtualizar(Cliente cliente){
+        EntityManager em = Conexao.getEntityManager();
+                      em.getTransaction().begin();
+       if(cliente.getId() != null){
+           cliente = em.merge(cliente);
+       }
+       em.persist(cliente);
+       em.getTransaction().commit();
+       em.close();
+    }
+    
+    
+     public void excluir(Cliente cliente){
+        EntityManager em = Conexao.getEntityManager();
+                      em.getTransaction().begin();
+                      cliente = em.merge(cliente);
+                      em.remove(cliente);
+                      em.getTransaction().commit();
+                      em.close();
+     
+     }
+     
+     public List<Cliente> pesquisar(Cliente cliente){
+         EntityManager em = Conexao.getEntityManager();
+         StringBuilder sql = new StringBuilder("from cliente c where 1=1");
+         
+         if (cliente.getId()!=null) {
+             sql.append("and c.id like :id ");
+         }
+         if (cliente.getNome()!=null && !cliente.getNome().equals("")) {
+             sql.append("and c.nome like :nome ");             
+         }
+         if (cliente.getEmail()!=null && !cliente.getEmail().equals("")) {
+             sql.append("and c.email like :email ");             
+         }
+         Query query = em.createQuery(sql.toString());
+         
+         if (cliente.getId() !=null) {
+             query.setParameter("id", cliente.getId());
+         }
+         if (cliente.getNome() !=null && !cliente.getNome().equals("")) {
+             query.setParameter("nome","%" + cliente.getNome());             
+         }
+         if (cliente.getEmail() !=null && !cliente.getEmail().equals("")) {
+             query.setParameter("email","%" + cliente.getEmail());
+         }
+         return query.getResultList();
+     }
+}
---
+++ Exer-Persistencia/Ecommerce_CES/src/DAO/Conexao.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author alunoces
+ */
+public class Conexao {
+    public static EntityManagerFactory emf;
+    private static Conexao conexao;
+    
+    
+    private Conexao(){
+        emf = Persistence.createEntityManagerFactory("Ecommerce_CESPU");
+    }
+    
+    public synchronized static EntityManager getEntityManager(){
+        if(conexao == null){
+            conexao = new Conexao();
+        }
+          return emf.createEntityManager();
+    }
+    
+}
