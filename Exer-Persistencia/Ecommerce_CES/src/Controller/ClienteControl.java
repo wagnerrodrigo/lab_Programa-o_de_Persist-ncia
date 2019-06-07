b0946791498b9a9612c55c8f3fb4321277db2006
Wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2019-06-06 21:41:00 -0300

    ClienteControl

---
+++ Exer-Persistencia/Ecommerce_CES/src/Controller/ClienteControl.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import DAO.ClienteDao;
+import Dominio.Cliente;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import java.util.List;
+import org.jdesktop.observablecollections.ObservableCollections;
+
+/**
+ *
+ * @author alunoces
+ */
+public class ClienteControl {
+
+    private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+    private Cliente clienteDigitado;
+    private Cliente clienteSelecionado;
+    private List<Cliente> clientesTabelas;
+    private ClienteDao clientedao;
+    
+    public Cliente getClienteDigitado(){
+        return clienteDigitado;
+    }
+    
+    public void setClienteDigitado(Cliente clienteDigitado){
+        Cliente oldClienteDigitado = this.clienteDigitado;
+        this.clienteDigitado = clienteDigitado;
+        propertyChangeSupport.firePropertyChange("clienteDigitado",oldClienteDigitado,clienteDigitado);        
+    }
+    
+    public Cliente getClienteSelecionado(){
+        return clienteSelecionado;
+    }
+    
+    public void setClienteSelecionado(Cliente clienteSelecionado){
+        if (this.clienteSelecionado !=null) {
+            setClienteDigitado(clienteSelecionado);            
+        }
+        this.clienteSelecionado = clienteSelecionado;
+    }
+    
+    public List<Cliente> getClientesTabelas(){
+        return clientesTabelas;
+    }
+    
+    public void setClientesTabelas(List<Cliente> clientesTabelas){
+        this.clientesTabelas = clientesTabelas;
+    }
+    
+    public ClienteControl(){
+        clientedao = new ClienteDao();
+        clientesTabelas = ObservableCollections.observableList(new ArrayList<Cliente>());
+        novo();
+        pesquisar();
+    }
+    
+    public void salva(){
+        clientedao.salvarAtualizar(clienteDigitado);
+        novo();
+        pesquisar();
+    }
+    
+    public void excluir(){
+        clientedao.excluir(clienteDigitado);
+        novo();
+        pesquisar();
+    }
+    
+    public final void novo(){
+        setClienteDigitado(new Cliente());
+    }
+    
+    public final void pesquisar(){
+        clientesTabelas.clear();
+        clientesTabelas.addAll(clientedao.pesquisar(clienteDigitado));
+    }
+    
+    public void addPropertyChangeListener(PropertyChangeListener e){
+        propertyChangeSupport.addPropertyChangeListener(e);
+    }
+    
+    public void removePropertyChangeListener(PropertyChangeListener e){
+        propertyChangeSupport.removePropertyChangeListener(e);
+    }
+    
+}
